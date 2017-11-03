ec9dcd3306f5d4e17aea88c6df6f3d05c353397f
GamzeAkbal1 <gakbal112@gmail.com>
2017-11-03 16:10:15 +0300

    Click Event Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Hello World zaaaaaaaaaaaaaaaaaaaaa!';
   username = '';
+
+  addUsername(username: string){
+    console.log(username);
+  }
 }
