9f38f13652dadf087abd57997434127d28ef3407
GamzeAkbal1 <gakbal112@gmail.com>
2017-11-03 14:23:03 +0300

    More Component :zap:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { NavigationComponent } from './app.navigation';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
