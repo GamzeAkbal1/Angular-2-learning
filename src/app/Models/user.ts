781d778add33bff0ae5ea4295dd47fb8ffad1238
GamzeAkbal1 <gakbal112@gmail.com>
2017-11-13 17:53:21 +0300

    PhoneNumber property added User Model :sparkles:

--- src/app/Models/user.ts
+++ src/app/Models/user.ts
@@ -1,8 +1,10 @@
 //export class for user field name
 export class UserModel {
   public name : string;
+  public phoneNumber : number;
 
-  constructor(name : string) {
+  constructor(name : string, phoneNumber : number) {
     this.name = name;
+    this.phoneNumber = phoneNumber;
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,13 @@ import { UserModel } from './Models/user';
 export class AppComponent {
   title = 'Hello World zaaaaaaaaaaaaaaaaaaaaa!';
   username = '';
-  userList: UserModel [] = [new UserModel('Ders1'),new UserModel('Ders2'),new UserModel('Ders3')];
+  phoneNumber ;
+  userList: UserModel [] = [new UserModel('Ders1',505792),new UserModel('Ders2',505785),new UserModel('Ders3',505649)];
   count = this.userList.length;
-  addUsername(username: string){
-    this.userList.push(new UserModel(username));
+  addUsername(username: string, phoneNumber : number){
+    this.userList.push(new UserModel(username,phoneNumber));
     this.username = '';
+    this.phoneNumber = null;
     this.count++;
   }
 
